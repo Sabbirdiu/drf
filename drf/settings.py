e3de128124ca35d2ec7f1de07a0f6850c865cd3e
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-06-19 21:17:54 +0600

    update settings

--- drf/settings.py
+++ drf/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quickstart',
+    'quickstart.apps.QuickstartConfig',
     'rest_framework',
 ]
 
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 10
 }
